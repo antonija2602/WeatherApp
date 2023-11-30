b594d0da3d8f8d7ce6b0eec932972e5138e19103
Antonija Zrno <antonija.zrno@gmx.de>
2023-11-30 16:29:15 +0100

    getbyid

--- script.js
+++ script.js
@@ -1,56 +1,45 @@
 const apiKey = "apiKey"
-            const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
-            const searchBox = document.querySelector(".search input")
-            const searchBtn = document.querySelector(".search button")
-            const weatherIcon = document.querySelector(".weather-icon")            
+const searchBox = document.getElementById("search-input")
+const searchBtn = document.getElementById("search-button")
+const weatherIcon = document.getElementById("weather-icon")
 
-            async function checkWeather(city) {
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
 
-                const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
+    if (response.status == 404) {
+        document.getElementById("error").style.display = "block"
+        document.getElementById("weather").style.display = "none"
+    } else {
+        let data = await response.json()
 
-                if(response.status==404){
-                    document.querySelector(".error").style.display="block"
-                    document.querySelector(".weather").style.display="none"
-                }
-                else
-                {        
-                let data = await response.json()
+        document.getElementById("city").innerHTML = data.name
+        document.getElementById("temp").innerHTML = Math.round(data.main.temp) + "°C"
+        document.getElementById("humidity").innerHTML = data.main.humidity + "%"
+        document.getElementById("wind").innerHTML = Math.round(data.wind.speed) + " km/h"
 
-                document.querySelector(".city").innerHTML = data.name
-                document.querySelector(".temp").innerHTML =Math.round(data.main.temp) + "°C"
-                document.querySelector(".humidity").innerHTML =data.main.humidity + "%"
-                document.querySelector(".wind").innerHTML =Math.round(data.wind.speed) + " km/h"
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "Images/clouds.png"
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "Images/rain.png"
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "Images/drizzle.png"
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "Images/clear.png"
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "Images/mist.png"
+        } else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "Images/snow.png"
+        }
+        document.getElementById("weather").style.display = "block"
+        document.getElementById("error").style.display = "none"
+    }
+}
 
-                if (data.weather[0].main == "Clouds"){
-                    weatherIcon.src = "Images/clouds.png"
-                }
-                else if(data.weather[0].main == "Rain"){
-                    weatherIcon.src = "Images/rain.png"
-                }
-                else if(data.weather[0].main == "Drizzle"){
-                    weatherIcon.src = "Images/drizzle.png"              
-                }
-                else if(data.weather[0].main == "Clear"){
-                    weatherIcon.src = "Images/clear.png"              
-                }
-                else if(data.weather[0].main == "Mist"){
-                    weatherIcon.src = "Images/mist.png"              
-                }            
-                else if(data.weather[0].main == "Snow"){
-                    weatherIcon.src = "Images/snow.png"              
-                }
-                document.querySelector(".weather").style.display="block"
-                document.querySelector(".error").style.display="none"
-                }}
-
-            searchBtn.addEventListener("click", function(){
-                checkWeather(searchBox.value)
-            })
-            searchBox.addEventListener("keyup", function(e){
-                if (e.keyCode === 13){
-                    checkWeather(searchBox.value)
-                }
-            })
-            
-           
+searchBtn.addEventListener("click", function () {
+    checkWeather(searchBox.value)
+})
+searchBox.addEventListener("submit", function () {
+    checkWeather(searchBox.value)
+})
